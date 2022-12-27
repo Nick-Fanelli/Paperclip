6149aafebd4d3bdc84712a76357fd623d7c51992
Nick Fanelli <nickfanelli5@gmail.com>
2022-12-27 10:46:52 -0500

    feat: add vector utils

--- PaperclipEngine/src/main/kotlin/com/paperclip/engine/graphics/camera/OrthographicCamera.kt
+++ PaperclipEngine/src/main/kotlin/com/paperclip/engine/graphics/camera/OrthographicCamera.kt
@@ -1,6 +1,42 @@
 package com.paperclip.engine.graphics.camera
 
+import com.paperclip.engine.math.Vector3f
+
+
+class ObservableVector3f(val observer: () -> Unit, _x: Float = 0.0f, _y: Float = 0.0f, _z: Float = 0.0f) {
+
+    constructor(observer: () -> Unit, value: Float) : this(observer, value, value, value)
+    constructor(observer: () -> Unit, vector3f: Vector3f) : this(observer, vector3f.x, vector3f.y, vector3f.z)
+
+    var x: Float = _x
+        set(value) {
+            field = value
+            observer()
+        }
+
+    var y: Float = _y
+        set(value) {
+            field = value
+            observer()
+        }
+
+    var z: Float = _z
+        set(value) {
+            field = value
+            observer()
+        }
+
+    fun set(x: Float, y: Float, z: Float) {
+        this.x = x
+        this.y = y
+        this.z = z
+    }
+
+}
+
 class OrthographicCamera : Camera() {
+
+    val position: ObservableVector3f = ObservableVector3f(::calculateViewMatrix)
 
     var zoom: Float = 1.0f
         set(value) {
--- PaperclipEngine/src/main/kotlin/com/paperclip/engine/math/Vector3f.kt
+++ PaperclipEngine/src/main/kotlin/com/paperclip/engine/math/Vector3f.kt
@@ -18,6 +18,12 @@ class Vector3f {
         this.z = z
     }
 
+    constructor(vec3f: org.joml.Vector3f) {
+        this.x = vec3f.x
+        this.y = vec3f.y
+        this.z = vec3f.z
+    }
+
     constructor(x: Float = 0.0f, y: Float = 0.0f, z: Float = 0.0f) {
         this.x = x
         this.y = y
@@ -39,5 +45,15 @@ class Vector3f {
     fun toVector2f() : Vector2f {
         return Vector2f(this.x, this.y)
     }
+
+    operator fun plus(value: Float) = Vector3f(this.x + value, this.y + value, this.z + value)
+    operator fun minus(value: Float) = Vector3f(this.x - value, this.y - value, this.z - value)
+    operator fun times(value: Float) = Vector3f(this.x * value, this.y * value, this.z * value)
+    operator fun div(value: Float) = Vector3f(this.x / value, this.y / value, this.z / value)
+    
+    operator fun plus(value: Vector3f) = Vector3f(this.x + value.x, this.y + value.y, this.z + value.z)
+    operator fun minus(value: Vector3f) = Vector3f(this.x - value.x, this.y - value.y, this.z - value.z)
+    operator fun times(value: Vector3f) = Vector3f(this.x * value.x, this.y * value.y, this.z * value.z)
+    operator fun div(value: Vector3f) = Vector3f(this.x / value.x, this.y / value.y, this.z / value.z)
 
 }
